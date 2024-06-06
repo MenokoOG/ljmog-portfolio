0d91bdcc23126a18bec0aaca0c165c082dd8a2b1
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-06-05 17:18:36 -0700

    update: 6-5-24

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,13 +11,13 @@ const NavBar = ({ onLinkClick }) => {
   };
 
   return (
-    <nav className="bg-blue-800 text-white fixed top-0 w-full z-10">
+    <nav className="bg-opacity-70 bg-black text-white fixed top-0 w-full z-10">
       <ul className="container mx-auto flex justify-around py-4">
-        <li><Link to="/" className="hover:text-blue-300" onClick={() => handleLinkClick('/')}>Home</Link></li>
-        <li><Link to="/projects" className="hover:text-blue-300" onClick={() => handleLinkClick('/projects')}>Projects</Link></li>
-        <li><Link to="/blog" className="hover:text-blue-300" onClick={() => handleLinkClick('/blog')}>Blog</Link></li>
-        <li><Link to="/portfolio" className="hover:text-blue-300" onClick={() => handleLinkClick('/portfolio')}>AI Art</Link></li>
-        <li><Link to="/contact" className="hover:text-blue-300" onClick={() => handleLinkClick('/contact')}>Contact</Link></li>
+        <li><Link to="/" className="hover:text-yellow-500" onClick={() => handleLinkClick('/')}>Home</Link></li>
+        <li><Link to="/projects" className="hover:text-yellow-500" onClick={() => handleLinkClick('/projects')}>Projects</Link></li>
+        <li><Link to="/blog" className="hover:text-yellow-500" onClick={() => handleLinkClick('/blog')}>Blog</Link></li>
+        <li><Link to="/portfolio" className="hover:text-yellow-500" onClick={() => handleLinkClick('/portfolio')}>AI Art</Link></li>
+        <li><Link to="/contact" className="hover:text-yellow-500" onClick={() => handleLinkClick('/contact')}>Contact</Link></li>
       </ul>
     </nav>
   );
