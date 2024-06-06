797d0ceb644a1d7476e89ac722f93784872016e5
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-06-05 17:06:47 -0700

    updates: 6-5-24

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import jeffImg from '../assets/l_jefferson.png';
+import './Home.css'; // Make sure to create this CSS file
 
+const techStack = [
+  { name: 'JavaScript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { name: 'HTML', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+  { name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+  { name: 'Python', url: 'https://www.python.org/doc/' },
+  { name: 'React', url: 'https://reactjs.org/docs/getting-started.html' },
+  { name: 'Node', url: 'https://nodejs.org/en/docs/' },
+  { name: 'Express', url: 'https://expressjs.com/en/starter/installing.html' },
+  { name: 'Nx Dev', url: 'https://nx.dev/latest/react/getting-started/intro' },
+  { name: 'Nest.js', url: 'https://nestjs.com/' },
+  { name: 'TypeScript', url: 'https://www.typescriptlang.org/docs/' },
+  { name: 'MongoDB', url: 'https://docs.mongodb.com/' },
+  { name: 'Firebase', url: 'https://firebase.google.com/docs' },
+  { name: 'Vite', url: 'https://vitejs.dev/guide/' }
+];
 
 const Home = () => {
   return (
     <main
       id="top"
       className="bg-cover bg-center min-h-screen"
-      
     >
       <div className="container mx-auto flex flex-wrap items-center bg-opacity-70 bg-black p-8 rounded-lg">
         <div className="w-full md:w-1/3 mb-8 md:mb-0 flex justify-center">
@@ -67,6 +82,23 @@ const Home = () => {
             custom JavaScript for auto-populating the imageData file for
             rendering.
           </p>
+          <div className="tech-stack">
+            <h2 className="text-3xl font-bold mb-4">My Tech Stack:</h2>
+            <ul className="text-yellow-100 swirl-text">
+              {techStack.map((tech, index) => (
+                <li key={index} className="tech-item">
+                  <a
+                    href={tech.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                  >
+                    {tech.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </main>
